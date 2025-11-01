094bece59fdd033e48f8b679579931f7f181cca4
RubenBaltasar <cacukq@hotmail.com>
2025-11-01 15:58:53 +0100

    problemas solucionados

--- Main.java
+++ Main.java
@@ -3,10 +3,8 @@ public class Main {
 	public static void main(String[] args) {
 
 	System.out.println("Versión inicial del proyecto.");
-<<<<<<< HEAD
 	System.out.println("Funcionalidad de login implementada.");
-=======
 	System.out.println("Funcionalidad de registro implementada.");
->>>>>>> feature/registro
+
 	}
 }
